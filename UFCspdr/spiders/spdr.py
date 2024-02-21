f9c0ff4ed304532f77ae1b6483e85f04bf4cf532
pollyjuice74 <hernandez.aht82836@gmail.com>
2024-02-21 11:53:40 -0400

    FighterGraph for elo ranking

--- UFCspdr/spiders/spdr.py
+++ UFCspdr/spiders/spdr.py
@@ -1,27 +1,59 @@
 # https://www.ufcespanol.com/athlete/raoni-haruserosu
+# scrapy shell 'https://www.ufcespanol.com/athlete/raoni-barcelos'
+# For using xpath https://doc.scrapy.org/en/latest/topics/selectors.html
+
 import scrapy as s
+import re
+from collections import defaultdict
 
 class ufcSpdr(s.Spider):
     name = "ufc"
     base_url = "https://www.ufcespanol.com"
     allowed_domains = [base_url]
 
-    start_urls = [base_url+"events"]
-    fighters = [base_url+"athlete/raoni-haruserosu"]
-    event = [base_url+"event/ufc-299"]
-    count = 0
+    start_url = [base_url+"/events"]
+    fighters = set() #[base_url+"/athlete/raoni-haruserosu"]
+    event = set() #[base_url+"/event/ufc-299"]
+
 
     def parse(self, response):
-        # Starts crawl
-        url = response.url
-
-        if url in self.fighter:
-            self.logger.info(f"Updated {url}")
-            return
-        else:
-            self.fighter.add(url)
-            count += 1
+        pass
         
+
+    def parseFighter(self, response):
+        # Starts crawl
+        fighter_url = response.url
+
+        #############################################   
+        text = ''.join(response.xpath("//p").getall())  
+
+        # Name
+        name = response.xpath("//h1[@class='hero-profile__name']/text()").getall()[0]
+        
+        # Gets record (W, L, D)
+        record_pattern = re.compile(r'(\d+)-(\d+)-(\d+) \(W-L-D\)')
+        record = re.findall(record_pattern, text)[0]
+        wins, losses, draws = int(record[0]), int(record[1]), int(record[2]) 
+
+        # Opponents
+        opponents = set(response.xpath("//h3/a/@href").getall()) - {fighter_url}
+        
+
+
+
+        # submission_pattern = re.compile(r'Wins by Submission</p>', re.IGNORECASE)
+        # method = re.findall(submission_pattern, text)
+
+
+        # striking_pattern = re.compile(r'<p><strong>Favorite Striking technique: </strong>([^<]+)</p>', re.IGNORECASE)
+        # submission_names = defaultdict(int)  # Using defaultdict to automatically handle new submissions
+        # striking_techniques = defaultdict(int)  # Similar for striking techniques
+
+        #############################################
+
+
+
+        # From the parsed history of upcoming fights, check each of the two fighters fighting career
         yield {
             "name": name,
 
@@ -29,23 +61,49 @@ class ufcSpdr(s.Spider):
             "losses": losses,
             "draws": draws,
 
-            "strikes": strikes,
-            "strikes_landed": strikes_landed,
-
-            "takedowns": takedowns,
-            "takedowns_landed": takedowns_landed,
-
-            "win_method": method,
-
+            "opponents": opponents, # {name: list of w/l/d}
             
+            # "strikes": strikes,
+            # "strikes_landed": strikes_landed,
+
+            # "takedowns": takedowns,
+            # "takedowns_landed": takedowns_landed,
+
+            # "win_method": method,
         }
 
+        # # Check if you ha
+        # if fighter_url in self.fighter:
+        #     self.logger.info(f"Updated {fighter_url}")
+        #     return
+        # else:
+        #     self.fighter.add(fighter_url)
 
-    def parseFighter():
-        # From the parsed history of upcoming fights, check each of the two fighters fighting career
-        pass
 
-    def checkHistory():
-        # Checks the start UFC url for upcoming fights
-        pass
+        # if fighter_url in self.start_urls:
+   
+        #     for fighter in self.fighters:
+        #         yield s.Request(fighter, callback=self.parse)
+
+
+
+
+    def checkHistory(self, response):
+        # Assuming https://www.ufcespanol.com/events
+        text = ''.join(response.xpath("//a/text()").getall())
+
+        # Date (m,d,y)
+        date_pattern = r"(\w+), (\w+) (\d+) / (\d+:\d+ [AP]M) (-\d+) /"
+        dates = re.findall(date_pattern, text)
+        # add year
+
+        # Fighters (F_1, F_2)
+        fighter_pattern = r"([A-Za-z' ]+) vs ([A-Za-z' ]+)"
+        fighters = re.findall(fighter_pattern, text)
+
+        # Links of fighters
+        link_pattern = r"/athlete/([a-z\-]+)"
+        links = re.findall(link_pattern, text)
+        links = [self.base_url + link for link in links]
+        # correct fighter names
 
--- rank.py
+++ rank.py
@@ -2,7 +2,25 @@
 
 class Fighter:
     def __init__(self, data):
-        self.data = dict()
+
+        # Data
+        ###############################################
+        self.data = data ### dict()
+
+        self.name = data.get('name', '')  # Fighter's name
+        self.wins = data.get('wins', 0)  # Total number of wins
+        self.losses = data.get('losses', 0)  # Total number of losses
+        self.draws = data.get('draws', 0)  # Total number of draws
+
+        self.strikes = data.get('strikes', 0)  # Total number of strikes attempted
+        self.strikes_landed = data.get('strikes_landed', 0)  # Total number of strikes that landed
+
+        self.takedowns = data.get('takedowns', 0)  # Total number of takedown attempts
+        self.takedowns_landed = data.get('takedowns_landed', 0)  # Total number of successful takedowns
+
+        # Win methods ("Submission", "KO", "Decision")
+        self.win_method = data.get('win_method', {})  # Example: {"Submission": {"Rear Naked Choke": 2, "Guillotine": 1}, "KO": 5}
+        ###############################################
 
         self.wins = 0
         self.losses = 0
@@ -14,20 +32,27 @@ class Fighter:
                         self.losses,
                         self.draws]
 
+
 class Fight:
     def __init__(self, data):
-        self.f1 = Fighter()
-        self.f2 = Fighter()
+        self.f0 = Fighter(data.fighters[0])
+        self.f1 = Fighter(data.fighters[1])
 
         self.winner 
         self.method
         self.date
 
+
+# Create graph of fighters
+        # Edges = {(u,v) | u,v elems of Vertices}
+        # Vertices = {fighter | fighter in fighters}
+        # fighters = set of all fighters
+        # Each edge should contain the W,L,D outcome of the fight
 def elo_rank(fighter, damp=400):
     w, l, d = fighter.record
     games = w+l+d
 
     rating = op_rating * damp*(w-l) / games
 
-    1/(1+10**(R_b-R_a)/400)# Predict outcome of game
+    1/(1+10**(R_b-R_a)/400) # Predict outcome of game
     rating = rating + 32(score-exp) # Update ratings
